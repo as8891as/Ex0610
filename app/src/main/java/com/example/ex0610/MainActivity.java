b5af1a9bdc835bfa63690c20c7562767999af889
as8891as <as8891@bs.amalnet.k12.il>
2025-04-21 21:32:52 +0300

    Task finished

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.ex0610;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Switch sw;
+    ToggleButton tB;
+    ConstraintLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sw = findViewById(R.id.sw);
+        tB = findViewById(R.id.tB);
+        main = findViewById(R.id.main);
+    }
+
+    public void pressed(View view) {
+        if (sw.isChecked() && tB.isChecked())
+        {
+            main.setBackgroundColor(Color.GREEN);
+        }
+        else if (sw.isChecked())
+        {
+            main.setBackgroundColor(Color.YELLOW);
+        }
+        else if (tB.isChecked())
+        {
+            main.setBackgroundColor(Color.BLUE);
+        }
+        else
+        {
+            main.setBackgroundColor(Color.RED);
+        }
     }
 }
